19cd6dd0c91a8704544f877c0e0903bfff144547
Yeonjeong Shin <87056545+shinyj1@users.noreply.github.com>
2022-05-29 03:01:26 +0900

    modify schedule
    1. 수정된 layout에 맞춰 일정 추가 관련 css 수정
    2. index에서 일정 추가 버튼 클릭시 schedule1으로 이동하도록 함
    3. schedule1.ejs 에서 schedule.js 적용 안되는 문제 해결
    4. ejs 태그 오류 해결

--- public/js/schedule1.js
+++ public/js/schedule1.js
@@ -1,41 +1,13 @@
 var infoOption = document.querySelector("#minWage");
-//체크된 경우 -> 최저시급 값을 wage input에 넣기
-if (infoOption.checked == true) {
-    var price = document.querySelector("#minWage").value;
-    document.querySelector("#wage").value = price;
-}
-//체크해제한 경우 -> 시급 칸 비워주기
-else { 
-    document.querySelector("#wage").value = "";
-}
 
+infoOption.addEventListener("click", function () { //checkbox에 이벤트 리스너 등록
+    if (infoOption.checked == true) { //체크된 경우 -> 최저시급 값을 wage input에 넣기
 
-<% for (var i = 0; i < 49; i++) { %>
-    <% var hour = ''; %>
-    <% var min = '00'; %>
+        var price = document.querySelector("#minWage").value;  //최저시급
 
-    <% if ((Math.ceil(<%= i %> / 2)) < 13) { %>
-        <% hour = (Math.floor(<%= i %> / 2)); %>
-    <% } else { %>
-        <% hour = (Math.floor(<%= i %> / 2)); %>
-    <% } %>
-
-    <% hour = (Math.floor(<%= i %> / 2)); %>
-
-    <% if (<%= hour %> < 10) { %>
-        <% hour = '0' + <%= hour %>; %>
-    <% } %>
-
-    <% if (<%= i %> % 2 != 0) { %>
-        <% min = '30'; %>
-    <% } %>
-    <script>
-    document
-        .write('<option value=' + <%= hour %> + <%= min %> + '>'
-            + <%= hour %>
-            + "시간 "
-            + <%= min %>
-            + "분"
-            + '</option>');
-            </script>
-<% } %>
+        document.querySelector("#wage").value = price;
+    }
+    else { //체크해제한 경우 -> 시급 칸 비워주기
+        document.querySelector("#wage").value = "";
+    }
+});

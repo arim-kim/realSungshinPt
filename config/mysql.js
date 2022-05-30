2ed2ddff6ad797498fe6c1d358f5b46875f5f096
kim-0722 <77314069+kim-0722@users.noreply.github.com>
2022-05-30 18:37:48 +0900

    ddf
    fdf

--- config/mysql.js
+++ config/mysql.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 const mysql = require("mysql2/promise");
 dotenv.config(); 
-
+////
 const connect = {
     init: function() {          // #2
         return mysql.createPool({

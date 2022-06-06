e7e2078e308e849cc57c5f453161920b201f7608
Yeonjeong Shin <87056545+shinyj1@users.noreply.github.com>
2022-06-06 13:41:02 +0900

    config.js / timezone 수정
    db에 원래 시간 찍히게 수정

--- config/config.js
+++ config/config.js
@@ -28,20 +28,23 @@ module.exports = {
     "password": process.env.DB_PW,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "timezone": "+09:00"
   },
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PW,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "timezone": "+09:00"
   },
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PW,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "timezone": "+09:00"
   }
 }
